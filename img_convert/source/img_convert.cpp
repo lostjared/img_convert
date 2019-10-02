3c613ecdef30eff998cc603491cf3ed0bd7cdf0e
Jared Bruni <jaredbruni@gmail.com>
2019-10-02 06:38:30 -0700

    added unix style argument

--- img_convert/source/img_convert.cpp
+++ img_convert/source/img_convert.cpp
@@ -3,12 +3,14 @@
 #include<string>
 #include<dirent.h>
 #include<sys/stat.h>
+#include<unistd.h>
 #include<opencv2/opencv.hpp>
 
 void add_directory(std::string path, std::string file_type, std::vector<std::string> &files) {
     DIR *dir = opendir(path.c_str());
     if(dir == NULL) {
-        std::cerr << "Error could not open directory: " << path << "\n";
+        std::cerr << "img_convert: Error could not open directory: " << path << "\n";
+        exit(EXIT_FAILURE);
         return;
     }
     dirent *file_info;
@@ -35,7 +37,7 @@ void add_directory(std::string path, std::string file_type, std::vector<std::str
                 std::string ext = f_info.substr(pos+1, f_info.length()-pos);
                 std::string filename = f_info.substr(0, pos);
                 if(ext == file_type) {
-                    std::cout << "added: " << fullpath << "\n";
+                    std::cout << "img_convert: added: " << fullpath << "\n";
                     files.push_back(fullpath);
                     continue;
                 }
@@ -45,33 +47,46 @@ void add_directory(std::string path, std::string file_type, std::vector<std::str
 }
 
 int main(int argc, char **argv) {
-    if(argc == 4) {
-        std::string path = argv[1];
-        std::string ext_from = argv[2];
-        std::string ext_to = argv[3];
-        std::vector<std::string> file_names;
-        add_directory(path,ext_from,file_names);
-        if(file_names.size()>0) {
-            for(unsigned int i = 0; i < file_names.size(); ++i) {
-                cv::Mat img;
-                img = cv::imread(file_names[i]);
-                if(img.empty()) {
-                    std::cerr << "Could not load image: " << file_names[i] << "\n";
-                    continue;
-                }
-                auto pos = file_names[i].rfind(".");
-                if(pos != std::string::npos) {
-                    std::string ext = file_names[i].substr(pos+1, file_names[i].length()-pos);
-                    std::string filename = file_names[i].substr(0, pos);
-                    std::ostringstream stream;
-                    stream << filename << "." << ext_to;
-                    cv::imwrite(stream.str(), img);
-                    std::cout << "[" << i << "/" << file_names.size() << "] - Converted Image: " << file_names[i] << " to: " << stream.str() << "\n";
-                }
+    std::string path = ".";
+    std::string ext_from = "jpg";
+    std::string ext_to = "png";
+    int opt = 0;
+    while((opt = getopt(argc, argv, "p:f:t:")) != -1) {
+        switch(opt) {
+            case 'p':
+                path = optarg;
+                break;
+            case 'f':
+                ext_from = optarg;
+                break;
+            case 't':
+                ext_to = optarg;
+                break;
+        }
+    }
+    std::vector<std::string> file_names;
+    add_directory(path,ext_from,file_names);
+    if(file_names.size()>0) {
+        for(unsigned int i = 0; i < file_names.size(); ++i) {
+            cv::Mat img;
+            img = cv::imread(file_names[i]);
+            if(img.empty()) {
+                std::cerr << "img_convert: Could not load image: " << file_names[i] << "\n";
+                continue;
+            }
+            auto pos = file_names[i].rfind(".");
+            if(pos != std::string::npos) {
+                std::string ext = file_names[i].substr(pos+1, file_names[i].length()-pos);
+                std::string filename = file_names[i].substr(0, pos);
+                std::ostringstream stream;
+                stream << filename << "." << ext_to;
+                cv::imwrite(stream.str(), img);
+                std::cout << "[" << i << "/" << file_names.size() << "] - Converted Image: " << file_names[i] << " to: " << stream.str() << "\n";
             }
         }
     } else {
-        std::cerr << "Invalid Arguments.. Use:\n" << argv[0] << " path type_from type_to\n";
+        std::cout << "img_convert: Found zero images in path: " << path << "\n";
     }
-	return 0;
+    return 0;
 }
+
